b42694fe3ded29ca82480fe6826e0e2c5ac5a36f
Metaxal <laurent.orseau@gmail.com>
2020-12-27 15:42:41 +0000

    ensure compatibility with Racket CS by using ffi-unsafe/port instead of make_fd_input_port

--- x11/fd.rkt
+++ x11/fd.rkt
@@ -7,6 +7,8 @@
 
 ;; opens an input port given a file descriptor.
 ;; (open-fd-input-port 1) ;; would open stdin
-(define open-fd-input-port
+#;(define open-fd-input-port
   (get-ffi-obj "scheme_make_fd_input_port" process-image
     (_fun (fd : _int) (_scheme = "fd-port") (_int = 0) (_int = 0) -> _racket)))
+(define (open-fd-input-port fd [name "fd-port"] [inta 0] [intb 0])
+  (error "deprecated. Use unsafe-file-descriptor->port instead."))
