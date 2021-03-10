97c4a75872cfd2882c8895bba88b87a4ad12be0e
Metaxal <laurent.orseau@gmail.com>
2021-03-10 15:22:41 +0000

    more explicit error message

--- x11/fd.rkt
+++ x11/fd.rkt
@@ -11,4 +11,4 @@
   (get-ffi-obj "scheme_make_fd_input_port" process-image
     (_fun (fd : _int) (_scheme = "fd-port") (_int = 0) (_int = 0) -> _racket)))
 (define (open-fd-input-port fd [name "fd-port"] [inta 0] [intb 0])
-  (error "deprecated. Use unsafe-file-descriptor->port instead."))
+  (error "open-fd-input-port is deprecated. Use unsafe-file-descriptor->port instead."))
